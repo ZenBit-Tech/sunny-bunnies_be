d52a25dec201921f884fc79df0b8c50173b2e826
jaroslawtt <yaroslavlogan61907@gmail.com>
2024-05-30 14:25:14 +0300

    fix: removed unnessary decorator

--- src/common/decorators/get-request-context.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const RequestContext = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => ctx.switchToHttp().getRequest(),
-);
