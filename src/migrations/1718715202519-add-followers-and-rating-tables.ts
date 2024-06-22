2b54db95d7c2b0dafdec0e3504072d3b0576c495
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-06-22 14:41:14 +0200

    fix: deleted extra code

--- src/migrations/1718715202519-add-followers-and-rating-tables.ts
+++ src/migrations/1718715202519-add-followers-and-rating-tables.ts
@@ -122,55 +122,6 @@ export class AddUserFollowers1718715202519 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const tableUserFollowers = await queryRunner.getTable(
-      TableName.USER_FOLLOWERS,
-    );
-    const tableUserRatings = await queryRunner.getTable(TableName.USER_RATINGS);
-
-    const foreignKeyUserFollowersUser = tableUserFollowers.foreignKeys.find(
-      (fk) => fk.columnNames.indexOf(ColumnName.USER_ID) !== -1,
-    );
-
-    const foreignKeyUserFollowersFollower = tableUserFollowers.foreignKeys.find(
-      (fk) => fk.columnNames.indexOf(ColumnName.FOLLOWER_ID) !== -1,
-    );
-
-    const foreignKeyUserRatingsRatedUser = tableUserRatings.foreignKeys.find(
-      (fk) => fk.columnNames.indexOf(ColumnName.RATED_USER_ID) !== -1,
-    );
-
-    const foreignKeyUserRatingsRatingUser = tableUserRatings.foreignKeys.find(
-      (fk) => fk.columnNames.indexOf(ColumnName.RATING_USER_ID) !== -1,
-    );
-
-    if (foreignKeyUserFollowersUser) {
-      await queryRunner.dropForeignKey(
-        TableName.USER_FOLLOWERS,
-        foreignKeyUserFollowersUser,
-      );
-    }
-
-    if (foreignKeyUserFollowersFollower) {
-      await queryRunner.dropForeignKey(
-        TableName.USER_FOLLOWERS,
-        foreignKeyUserFollowersFollower,
-      );
-    }
-
-    if (foreignKeyUserRatingsRatedUser) {
-      await queryRunner.dropForeignKey(
-        TableName.USER_RATINGS,
-        foreignKeyUserRatingsRatedUser,
-      );
-    }
-
-    if (foreignKeyUserRatingsRatingUser) {
-      await queryRunner.dropForeignKey(
-        TableName.USER_RATINGS,
-        foreignKeyUserRatingsRatingUser,
-      );
-    }
-
     await queryRunner.dropTable(TableName.USER_FOLLOWERS);
     await queryRunner.dropTable(TableName.USER_RATINGS);
   }
