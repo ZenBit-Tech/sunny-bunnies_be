fadb27dee7e59594554c9c3699c039ea8e1cc268
likeri29 <s.pochtaruk29@gmail.com>
2024-07-12 10:14:38 +0200

    fix: fix product module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { MailerService } from '@nestjs-modules/mailer';
 import { ProductEntity } from '~/entities';
 
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { ProductsRepository } from './products.repository';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService, ProductsRepository],
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity]),
+    UsersModule,
+    MailerService,
+  ],
   exports: [ProductsService],
 })
 export class ProductsModule {}
